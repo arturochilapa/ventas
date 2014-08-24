cccb43367d1ff631ffc8dd8d25c88c57c1d044a8
Arturo Hernandez <arturo.webrek@chilapa.mx>
2014-08-23 21:45:36 -0500

    Register Auth

--- app/controllers/UsersController.php
+++ app/controllers/UsersController.php
@@ -32,7 +32,12 @@ class UsersController extends \BaseController {
 	 */
 	public function create()
 	{
-		return View::make('users.register');
+        if (Auth::check()){
+            return Redirect::to('profile');
+        }else{
+            return View::make('users.register');
+        }
+		
 	}
 
 
